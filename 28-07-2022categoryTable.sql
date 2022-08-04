f42b0985caa380462e944cc7c0db1d4f5493731c
HimakarS <69719637+HimakarS@users.noreply.github.com>
2022-08-04 10:25:47 +0530

    Add files via upload

---
+++ 28-07-2022categoryTable.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from category
+LIMIT 0, 1000
+
+-- Date: 2022-08-04 10:23
+*/
+INSERT INTO `` (`categoryid`,`categoryname`) VALUES (123,'New category');
+INSERT INTO `` (`categoryid`,`categoryname`) VALUES (124,'New ');
+INSERT INTO `` (`categoryid`,`categoryname`) VALUES (456,'Hello');
+INSERT INTO `` (`categoryid`,`categoryname`) VALUES (1001,'Dairy');
+INSERT INTO `` (`categoryid`,`categoryname`) VALUES (1002,'Fruits');
---
+++ 28-07-2022productTable.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select * from product
+LIMIT 0, 1000
+
+-- Date: 2022-08-04 10:22
+*/
+INSERT INTO `` (`id`,`name`,`description`,`unitprice`,`imageurl`,`unitsinstock`,`datecreated`,`lastupdated`,`categoryid`) VALUES (101,'MilkUpdated2','Fresh milk updated',28.00,'https://www.bigbasket.com/media/uploads/p/l/1202684_3-amul-taaza-homogenised-toned-milk.jpg',100,'2022-07-28 00:08:07','2022-07-28 00:08:07',1001);
+INSERT INTO `` (`id`,`name`,`description`,`unitprice`,`imageurl`,`unitsinstock`,`datecreated`,`lastupdated`,`categoryid`) VALUES (102,'Curd','Fresh curd',40.00,'https://www.bigbasket.com/media/uploads/p/l/20003875_4-godrej-jersey-toned-milk-curd.jpg',70,'2022-07-28 00:08:43','2022-07-28 00:08:43',1001);
+INSERT INTO `` (`id`,`name`,`description`,`unitprice`,`imageurl`,`unitsinstock`,`datecreated`,`lastupdated`,`categoryid`) VALUES (103,'Apples','Red Apples',30.00,'https://www.bigbasket.com/media/uploads/p/s/40075384_12-fresho-apple-red-delicious-economy.jpg',100,'2022-07-28 00:09:59','2022-07-28 00:09:59',1002);
+INSERT INTO `` (`id`,`name`,`description`,`unitprice`,`imageurl`,`unitsinstock`,`datecreated`,`lastupdated`,`categoryid`) VALUES (107,'Watermelon','Watermelon desc',30.35,'https://www.bigbasket.com/media/uploads/p/l/40023482_8-fresho-watermelon-organically-grown.jpg',30,'2011-12-05 12:03:01','2011-12-05 12:03:01',1002);
+INSERT INTO `` (`id`,`name`,`description`,`unitprice`,`imageurl`,`unitsinstock`,`datecreated`,`lastupdated`,`categoryid`) VALUES (109,'Kiwi','New Zealand Kiwi',50.00,'https://www.bigbasket.com/media/uploads/p/s/20000911_30-fresho-kiwi-green.jpg',50,'2011-12-05 12:03:01','2011-12-05 12:03:01',1002);
+INSERT INTO `` (`id`,`name`,`description`,`unitprice`,`imageurl`,`unitsinstock`,`datecreated`,`lastupdated`,`categoryid`) VALUES (111,'Butter','cbdhchc',40.00,'https://www.bigbasket.com/media/uploads/p/s/1200163_4-amul-butter-pasteurized.jpg',40,'2022-07-28 22:30:05','2011-12-05 12:03:01',1001);
